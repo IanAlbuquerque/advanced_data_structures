9592b04b04d1df074bed6bec3cbf8ab64e1b8b93
Ian Albuquerque <ian.albuquerque.silva@gmail.com>
2014-11-25 15:58:53 -0200

    Name Variables

--- T2/grafos.cpp
+++ T2/grafos.cpp
@@ -7,221 +7,237 @@
 
 using namespace std;
 
-vector<int> graph[MAX_VERTICES];
-set<int> click; 
-int color_array[MAX_VERTICES]; //array that has the color of each vertex
-int N; //number of current vertex
-int degree[MAX_VERTICES];
-set<int> S[MAX_VERTICES];
+vector<int> adjacency_list[MAX_VERTICES];
+set<int> clique; 
+int vertex_color[MAX_VERTICES]; 
+int NUM_VERTICES; 
+int vertex_degree[MAX_VERTICES];
+set<int> clique_components[MAX_VERTICES];
 
-//returns the degree of the vertex
-int calculateDegrees(set<int>* vertices)
+
+void calculateDegrees(set<int>* vertices)
 {
-	int num_vizinhos;
-	for(int v=0;v<N;v++)
+	int num_adjacent_vertices;
+	for(int vertex=0; vertex<NUM_VERTICES; vertex++)
 	{
-		num_vizinhos = graph[v].size();
-		degree[v] = 0;
-		for(int i=0;i<num_vizinhos;i++)
+		num_adjacent_vertices = adjacency_list[vertex].size();
+		vertex_degree[vertex] = 0;
+		for(int i=0; i<num_adjacent_vertices; i++)
 		{
-			if(vertices->count(graph[v][i]))
+			if(vertices->count(adjacency_list[vertex][i]))
 			{
-				degree[v]++;
+				vertex_degree[vertex]++;
 			}
 		}
 	}
 }
 
-//finds the vertex with the biggest degree
-int vMaxDegree(set<int>* vertices)
+void calculateDegreesWholeGraph()
 {
-	int max=-1;
-	int v_max; //value to be returned
-	int size; //degree of the vertex
-	//loop while there's non visited vertex
-	for ( set<int>::iterator i = vertices->begin(); i != vertices->end(); i++ )
+	for(int i=0;i<NUM_VERTICES;i++)
 	{
-		if(degree[*i] > max)
-		{
- 			max = degree[*i];
- 			v_max = *i;
-		}
+		vertex_degree[i] = adjacency_list[i].size();
 	}
-	return v_max;
 }
 
-//looks for a edge between v and w vertex
+int vertexWithMaximumDegree(set<int>* vertices)
+{
+	int max_degree = -1;
+	int vertex_with_max_degree; 
+	
+	for (set<int>::iterator it=vertices->begin(); it!=vertices->end(); it++)
+	{
+		if(vertex_degree[*it] > max_degree)
+		{
+ 			max_degree = vertex_degree[*it];
+ 			vertex_with_max_degree = *it;
+		}
+	}
+	return vertex_with_max_degree;
+}
+
+
 bool isAdjacent(int v, int w)
 {
-
-	int num_arestas = degree[v];
-	//while there's edges checks for the w variable
-	for(int i=0;i<num_arestas;i++)
-		if(w == graph[v][i])
-			return true;
+	int num_adjacent_vertices = vertex_degree[v];
 	
+	for(int i=0; i<num_adjacent_vertices; i++)
+	{
+		if(w == adjacency_list[v][i])
+		{
+			return true;
+		}
+	}
+
 	return false;
 }
 
-//checks if v is adjacent to the click
-bool isAdjacentToClick(int v, set<int>* click)
+
+bool hasEdgesToEveryoneInClique(int vertex, set<int>* clique)
 {
-	int click_size = click->size();
-	//while theres vertex in click
-	for ( set<int>::iterator i = click->begin(); i != click->end(); i++ )
-		if(!isAdjacent(v,*i))
-			return false;
+	int click_size = clique->size();
 	
+	for (set<int>::iterator it=clique->begin(); it!=clique->end(); it++)
+	{
+		if(!isAdjacent(vertex,*it))
+		{
+			return false;
+		}
+	}
+
 	return true;
 }
 
-//finds if v is part of the click
-bool isClick(int v)
+
+bool isVertexInTheClique(int vertex, set<int>* clique)
 {
-	//check every vertex of the click and compare to v
-	for (set<int>::iterator i = click.begin(); i != click.end(); i++)
-		if(v==*i)
+	for (set<int>::iterator it=clique->begin(); it!=clique->end(); it++)
+	{
+		if(vertex==*it)
 			return true;
+	}
 	
 	return false;
 }
 
-void findClickPodeSolitario(set<int>* vertices)
+void findCliqueIncludingSoloVertex(set<int>* vertices)
 {
+	int vertex_with_max_degree;
+	int max_degree;
+
+	vector<int> adjacent_vertices;
+	int num_adjacent_vertices;
+
 	calculateDegrees(vertices);
-	//clear click cache
-	click.clear(); //a
-	int v = vMaxDegree(vertices); // b
-	click.insert(v);
+	clique.clear(); 
 
-	vector<int> v_adj;
-	//size of the adjacent vertex
-	int adj_size;
-	//max degree of the adjacent vertex
-	int adj_max_degree, max_degree;
-	//loop while there's adjacents vertex
-	while(1)
-	{
-		v_adj.clear(); // c
-		for ( set<int>::iterator i = vertices->begin(); i != vertices->end(); i++ )
-		{
-			//if the vertex is not in the click
-			if(!isClick(*i))
-			{
-				if(isAdjacentToClick(*i,&click))
-					//add i to the v_adj if its adjacent and is not part of the click
-					v_adj.push_back(*i);
-			}
-		}
-		
-
-		adj_size = v_adj.size();
-		//end loop if tehres no v_adj
-		if(!adj_size) break;
-
-		adj_max_degree = -1;
-		max_degree = 0;
-		//checks for v_adj	 vertex with the biggest degree
-		for(int i=0;i<adj_size;i++)
-			if(degree[v_adj[i]] > max_degree)
-			{
-				max_degree = degree[v_adj[i]];
-				adj_max_degree = v_adj[i];
-			}
-		
-		//add to click the biggest degree v_adj with an adjacent edge
-		click.insert(adj_max_degree);
-	}
-}
-
-void findClick()
-{
-
-	set<int> V;
-
-	for(int i=0;i<N;i++)
-	{
-		V.insert(i);
-	}
-
-	findClickPodeSolitario(&V);
-
-	if(click.size() == 1)
-	{
-		click.clear();
-	}
-}
-
-//color the graph's vertex
-void color(int v)
-{
-	for(int i=0;i<N;i++)
-	{
-		degree[i] = graph[i].size();
-	}
-
-	//array of already used colors in the adjacents' vertex
-	bool used_colors[MAX_VERTICES];
-	int num_adj = degree[v];
-
-	//initiate the value as false
-	for(int i=0;i<MAX_VERTICES;i++)
-		used_colors[i] = false;
+	vertex_with_max_degree = vertexWithMaximumDegree(vertices); 
+	clique.insert(vertex_with_max_degree);
 	
-	//checks for already used colors
-	for(int i=0;i<num_adj;i++)
+	while(true)
 	{
-		if(color_array[graph[v][i]] != -1)
+		adjacent_vertices.clear(); 
+		for (set<int>::iterator i=vertices->begin(); i!=vertices->end(); i++)
 		{
-			used_colors[color_array[graph[v][i]]] = true;
+			if(!isVertexInTheClique(*i,&clique))
+			{
+				if(hasEdgesToEveryoneInClique(*i,&clique))
+				{
+					adjacent_vertices.push_back(*i);
+				}
+			}
+		}
+		
+		num_adjacent_vertices = adjacent_vertices.size();
+		
+		if(!num_adjacent_vertices)
+		{
+			break;	
 		}
 
+		max_degree = -1;
+		vertex_with_max_degree = -1;
+		
+		for(int i=0; i<num_adjacent_vertices; i++)
+		{
+			if(vertex_degree[adjacent_vertices[i]] > max_degree)
+			{
+				max_degree = vertex_degree[adjacent_vertices[i]];
+				vertex_with_max_degree = adjacent_vertices[i];
+			}
+		}
+		
+		clique.insert(vertex_with_max_degree);
 	}
-	//checks for the lowest not used color
-	//eg: in this array [wite][grey][blue][][red] it would use the used_colors[3]
+}
+
+void findClique(set<int>* vertices)
+{
+	findCliqueIncludingSoloVertex(vertices);
+
+	if(clique.size() == 1)
+	{
+		clique.clear();
+	}
+}
+
+void findClickWholeGraph()
+{
+	set<int> vertices;
+
+	for(int i=0; i<NUM_VERTICES; i++)
+	{
+		vertices.insert(i);
+	}
+
+	findClique(&vertices);
+}
+
+
+void clearUsedColors(bool* adjacent_used_colors)
+{
+	for(int i=0; i<MAX_VERTICES; i++)
+	{
+		adjacent_used_colors[i] = false;
+	}
+}
+
+void clearVerticesColor()
+{
+	for(int i=0; i<NUM_VERTICES; i++)
+	{
+		vertex_color[i] = -1;
+	}
+}
+
+void setAdjacentUsedColors(int vertex, int num_adjacent_vertices, bool* adjacent_used_colors)
+{
+	for(int i=0;i<num_adjacent_vertices;i++)
+	{
+		if(vertex_color[adjacency_list[vertex][i]] != -1)
+		{
+			adjacent_used_colors[vertex_color[adjacency_list[vertex][i]]] = true;
+		}
+	}
+}
+
+void colorVertex(int vertex)
+{
+
+	bool adjacent_used_colors[MAX_VERTICES];
+	int num_adjacent_vertices;
+
+	calculateDegreesWholeGraph();
+
+	num_adjacent_vertices = vertex_degree[vertex];
+
+	clearUsedColors(adjacent_used_colors);
+	
+	setAdjacentUsedColors(vertex, num_adjacent_vertices, adjacent_used_colors);
+	
 	for(int i=0;i<MAX_VERTICES;i++)
 	{
-		if(!used_colors[i])
+		if(!adjacent_used_colors[i])
 		{
-			color_array[v] = i;
+			vertex_color[vertex] = i;
 			break;
 		}
 	}
-	//color every adjacents' vertex if its not colord
-	for(int i=0;i<num_adj;i++)
+
+	for(int i=0;i<num_adjacent_vertices;i++)
 	{
-		if(color_array[graph[v][i]] == -1)
+		if(vertex_color[adjacency_list[vertex][i]] == -1)
 		{
-			color(graph[v][i]);
+			colorVertex(adjacency_list[vertex][i]);
 		}
 	}
 }
-//init every color and start coloring
-void initAndColor(int v)
+
+void initAndColor(int vertex)
 {
-	for(int i=0;i<N;i++)
-		color_array[i] = -1;
-	
-	color(v);
+	clearVerticesColor();
+	colorVertex(vertex);
 }
-
-//==============================================
-
-/*
-2. encontra cobertura minimal :
-(a) Inicie com V' = V . Faça q = 1.
-(b) Encontre uma clique em G(V') correspondente ao conjunto S'q .
-(c) Faça V' ← V' \ S'q .
-(d) Se V' != ∅. Faça q ←− q + 1 e volte ao passo (b)
-
-(e) Caso contrário, para p de 1 a q Faça:
-i. Adicione a S'p um vértice que seja vizinho a todos os vértices já em S'p dentre estes,
-escolha vértice de maior grau.
-ii. Se nenhum vértice puder ser adicionado a S p , Pare! S p  ́e uma clique.
-iii. Faça S p = S p .
-(f) O conjunto S1 , . . . , S'q  ́e uma cobertura minimal em cliques.
-*/
-
 
 int cobertura_minimal()
 {
@@ -229,15 +245,15 @@ int cobertura_minimal()
 	set<int> v_linha;
 	set<int> candidates;
 	int max_degree = 0;
-	int adj_max_degree = -1;
+	int vertex_with_max_degree = -1;
 	int numero_de_coberturas;
 
-	for(int i=0;i<=N;i++)
+	for(int i=0;i<=NUM_VERTICES;i++)
 	{
-		S[i].clear();
+		clique_components[i].clear();
 	}
 
-	for(int i=0;i<N;i++)
+	for(int i=0;i<NUM_VERTICES;i++)
 	{
 		v_linha.insert(i);
 	}
@@ -247,10 +263,10 @@ int cobertura_minimal()
 	{
 		q++;
 
-		findClickPodeSolitario(&v_linha);
-		S[q] = click;
+		findCliqueIncludingSoloVertex(&v_linha);
+		clique_components[q] = clique;
 
-		for ( set<int>::iterator i = S[q].begin(); i != S[q].end(); i++ )
+		for ( set<int>::iterator i = clique_components[q].begin(); i != clique_components[q].end(); i++ )
 		{
 			v_linha.erase(*i);
 		}
@@ -260,18 +276,18 @@ int cobertura_minimal()
 	numero_de_coberturas = q;
 
 
-	for(int i=0;i<N;i++)
+	for(int i=0;i<NUM_VERTICES;i++)
 	{
-		degree[i] = graph[i].size();
+		vertex_degree[i] = adjacency_list[i].size();
 	}
 
 	for(int p=1;p<=q;p++)
 	{
 		candidates.clear();
 
-		for(int i=0;i<N;i++)
+		for(int i=0;i<NUM_VERTICES;i++)
 		{
-			if(isAdjacentToClick(i,&S[p]))
+			if(hasEdgesToEveryoneInClique(i,&clique_components[p]))
 			{
 				candidates.insert(i);
 			}	
@@ -280,18 +296,18 @@ int cobertura_minimal()
 		{
 
 			max_degree = 0;
-			adj_max_degree = -1;
+			vertex_with_max_degree = -1;
 			for ( set<int>::iterator i = candidates.begin(); i != candidates.end(); i++ )
 			{
-				if( degree[*i] > max_degree )
+				if( vertex_degree[*i] > max_degree )
 				{
-					max_degree = degree[*i];
-					adj_max_degree = *i;
+					max_degree = vertex_degree[*i];
+					vertex_with_max_degree = *i;
 				}
 			}
 
-			S[p].insert(adj_max_degree);
-			candidates.erase(adj_max_degree);
+			clique_components[p].insert(vertex_with_max_degree);
+			candidates.erase(vertex_with_max_degree);
 		}
 
 	}
@@ -302,12 +318,12 @@ int cobertura_minimal()
 
 void run_tudo(bool should_print)
 {
-	findClick();
+	findClickWholeGraph();
 	if(should_print) 
 	{
-		for ( set<int>::iterator i = click.begin(); i != click.end(); i++ )
+		for ( set<int>::iterator i = clique.begin(); i != clique.end(); i++ )
 			printf("%d\n",*i);
-		printf("Temanho do clique encontrado = %d\n",click.size());
+		printf("Temanho do clique encontrado = %d\n",clique.size());
 	}
 	
 
@@ -317,19 +333,19 @@ void run_tudo(bool should_print)
 	
 	if(should_print) 
 	{
-		for(int i=0;i<N;i++)
+		for(int i=0;i<NUM_VERTICES;i++)
 		{
-			printf("cor do %d = %d\n",i,color_array[i]);
+			printf("cor do %d = %d\n",i,vertex_color[i]);
 		}
 	}
 	
 
 	int max_color = -1;
-	for(int i=0;i<N;i++)
+	for(int i=0;i<NUM_VERTICES;i++)
 	{
-		if(color_array[i] > max_color)
+		if(vertex_color[i] > max_color)
 		{
-			max_color = color_array[i];
+			max_color = vertex_color[i];
 		}
 	}
 	if(should_print) printf("Numero de cores usados = %d\n",max_color+1);
@@ -345,20 +361,20 @@ void run_tudo(bool should_print)
 		num_clicks[i]=0;
 	}
 	int max_size_clique = -1;
-	int min_size_clique = N+1;
+	int min_size_clique = NUM_VERTICES+1;
 	int soma=0;
 	for(int i=1;i<=numero_de_coberturas;i++)
 	{
-		soma += (int) S[i].size();
-		if( (int) S[i].size() > max_size_clique)
+		soma += (int) clique_components[i].size();
+		if( (int) clique_components[i].size() > max_size_clique)
 		{
-			if(S[i].size() > 1) max_size_clique = S[i].size();
+			if(clique_components[i].size() > 1) max_size_clique = clique_components[i].size();
 		}
-		if( (int) S[i].size() < min_size_clique)
+		if( (int) clique_components[i].size() < min_size_clique)
 		{
-			if(S[i].size() > 1) min_size_clique = S[i].size();
+			if(clique_components[i].size() > 1) min_size_clique = clique_components[i].size();
 		}
-		num_clicks[S[i].size()] = num_clicks[S[i].size()] + 1;
+		num_clicks[clique_components[i].size()] = num_clicks[clique_components[i].size()] + 1;
 	}
 
 	float media = ((float)soma)/numero_de_coberturas;
@@ -378,34 +394,6 @@ void run_tudo(bool should_print)
 
 int main(int argn, char* argc[])
 {
-
-/*
-	N = 6;
-	graph[0].push_back(1);
-	graph[0].push_back(2);
-	graph[0].push_back(3);
-	graph[0].push_back(4);
-	graph[0].push_back(5);
-
-	graph[1].push_back(0);
-	graph[1].push_back(3);
-	graph[1].push_back(4);
-	graph[1].push_back(5);
-
-	graph[2].push_back(0);
-
-	graph[3].push_back(0);
-	graph[3].push_back(1);
-
-	graph[4].push_back(0);
-	graph[4].push_back(1);
-	graph[4].push_back(5);
-
-	graph[5].push_back(0);
-	graph[5].push_back(1);
-	graph[5].push_back(4);
-*/
-
 	char c_lido;
 	int M;
 	int v1,v2;
@@ -423,18 +411,18 @@ int main(int argn, char* argc[])
 		}
 	}
 
-	scanf(" %*s %d %d",&N,&M);
-	printf("N=%d M=%d\n",N,M);
+	scanf(" edge %d %d",&NUM_VERTICES,&M);
+	printf("NUM_VERTICES=%d M=%d\n",NUM_VERTICES,M);
 
 	for(int i=0;i<M;i++)
 	{
 		scanf(" e %d %d ",&v1, &v2);
-		graph[v1].push_back(v2);
-		graph[v2].push_back(v1);
-		//printf("%d %d\n",v1,v2);
+		adjacency_list[v1].push_back(v2);
+		adjacency_list[v2].push_back(v1);
+		
 	}
 
-	//====================================================
+	
 
 	CPUTimer timer_total;
 	int runs = 0;
@@ -450,7 +438,7 @@ int main(int argn, char* argc[])
 	}
 	while(timer_total.getCPUTotalSecs() < 5.0f);
 
-	printf("Executou %d execucoes em %fs (media: %f exec/s)\n",runs,timer_total.getCPUTotalSecs(),timer_total.getCPUTotalSecs()/runs);
+	printf("Executou %d execucoes em %fs (media: %f exec/clique_components)\n",runs,timer_total.getCPUTotalSecs(),timer_total.getCPUTotalSecs()/runs);
 
 	return 0;
 }
